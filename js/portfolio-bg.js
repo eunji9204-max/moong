a73ae0a5a0a3e6127077d5198aa7616512395421
eunji9204-max <eunji9204@gmail.com>
2026-07-13 00:40:19 +0900

    Update portfolio-bg.js

--- js/portfolio-bg.js
+++ js/portfolio-bg.js
@@ -252,3 +252,14 @@
   startSequence();
   render();
 })();
+
+
+const btnTop = document.querySelector('.btn-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    btnTop.classList.add('show');
+  } else {
+    btnTop.classList.remove('show');
+  }
+});
